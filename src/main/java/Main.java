755b643d37ae9d57f8ba4e1e0c2a5ebd0725d8af
Kendra <kendra@Zipcoders-MacBook-Pro-45.local>
2022-07-02 12:39:48 -0400

    main loop built

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-        
+        Scanner askGuess = new Scanner(System.in);
+        System.out.println("Guess a number from 1 to 10");
+        int userGuess = askGuess.nextInt();
+        int min = 1;
+        int max = 10;
+        double randomNumber = (Math.random() * (max - min)) + min;
+        while (userGuess != randomNumber){
+            if (userGuess > randomNumber){
+                System.out.println("Too Large");
+            } else {
+                System.out.println("Too Small");
+            }
+            System.out.println("Guess again");
+            userGuess = askGuess.nextInt();
+        }
+
+        System.out.println("Correct Guess");
     }
 }
