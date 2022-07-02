40ffe7b1f3bef5cbc3f546c7df16a4a819ecb4a3
Kendra <kendra@Zipcoders-MacBook-Pro-45.local>
2022-07-02 13:13:24 -0400

    added guess counter and last guess tracking

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,21 +8,29 @@ public class Main {
 
     public static void main(String[] args){
         Scanner askGuess = new Scanner(System.in);
-        System.out.println("Guess a number from 1 to 10");
+        System.out.println("Guess a number from 1 to 100");
         int userGuess = askGuess.nextInt();
-        int min = 1;
-        int max = 10;
-        double randomNumber = (int)(Math.random() * (max - min)) + min;
+        double randomNumber = (int)(Math.random() * (100 - 1)) + 1;
+
+        int attempts = 0;
+        int lastGuess = 101;
         while (userGuess != randomNumber){
-            if (userGuess > randomNumber){
+            if(userGuess == lastGuess) {
+                System.out.println("You already guessed that number");
+            }
+            else if (userGuess > randomNumber){
+                attempts ++;
                 System.out.println("Too Large");
+                lastGuess = userGuess;
             } else {
+                attempts ++;
                 System.out.println("Too Small");
+                lastGuess = userGuess;
             }
             System.out.println("Guess again");
             userGuess = askGuess.nextInt();
         }
-
-        System.out.println("Correct Guess");
+        attempts ++;
+        System.out.println("Correct Guess. It took you " + attempts + " tries.");
     }
 }
